6bda6dda04fd042c3f6d187a623d9d539493dd35
MiyuYokoyama0109 <miyu.y@latona.io>
2023-02-22 13:08:20 +0900

    release

--- data-platform-global-region-sql-global-region-data-setup.sql
+++ data-platform-global-region-sql-global-region-data-setup.sql
@@ -2,13 +2,13 @@ LOCK TABLES `data_platform_global_region_global_region_data` WRITE;
 
 INSERT INTO `data_platform_global_region_global_region_data` (`GlobalRegion`)
 VALUES
-    ('AF');
-    ('AS');
-    ('CB');
-    ('EU');
-    ('ME');
-    ('NA');
-    ('PC');
-    ('SA');
+    ('AF'),
+    ('AS'),
+    ('CB'),
+    ('EU'),
+    ('ME'),
+    ('NA'),
+    ('PC'),
+    ('SA'),
     ('WW');
 UNLOCK TABLES;
--- data-platform-global-region-sql-global-region-text-data-setup.sql
+++ data-platform-global-region-sql-global-region-text-data-setup.sql
@@ -2,14 +2,14 @@ LOCK TABLES `data_platform_global_region_global_region_text_data` WRITE;
 
 INSERT INTO `data_platform_global_region_global_region_text_data` (`GlobalRegion`, `Language`, `GlobalRegionName`)
 VALUES
-    ('AF', 'JA', 'Africa');
-    ('AS', 'JA', 'Asia');
-    ('CB', 'JA', 'Caribbean');
-    ('EU', 'JA', 'Europe');
-    ('ME', 'JA', 'Middle East'); 
-    ('NA', 'JA', 'North America'); 
-    ('PC', 'JA', 'Pacific');
-    ('SA', 'JA', 'South America'); 
+    ('AF', 'JA', 'Africa'),
+    ('AS', 'JA', 'Asia'),
+    ('CB', 'JA', 'Caribbean'),
+    ('EU', 'JA', 'Europe'),
+    ('ME', 'JA', 'Middle East'), 
+    ('NA', 'JA', 'North America'), 
+    ('PC', 'JA', 'Pacific'),
+    ('SA', 'JA', 'South America'), 
     ('WW', 'JA', 'Worldwide');
 UNLOCK TABLES;
 
